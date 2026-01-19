876131b447cea1c298477476ad63f68a636fd777
szkajvolker <ist9595@windowslive.com>
2026-01-19 17:56:20 +0100

    fix pagination bug,search text smaller on small devices,basic notify component,gsap

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,16 @@ import Content from "./components/Content.jsx";
 import { fetchMoviesData } from "./API/tmdbapi.js";
 import Navbar from "./components/Navbar.jsx";
 
+import { NotificationHandler } from "./components/Notification.jsx";
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [notification, setNotification] = useState({
+    text: "",
+    color: "",
+  });
 
   const handleNext = () => {
     const newPage = currentPage + 1;
@@ -31,6 +38,12 @@ function App() {
 
   return (
     <main>
+      <NotificationHandler
+        textToShow={notification.text}
+        color={notification.color}
+        delay={2000}
+        onClose={() => setNotification({ text: "", color: "" })}
+      />
       <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <div className="pattern" />
       <div className="wrapper">
@@ -40,8 +53,14 @@ function App() {
           currentPage={currentPage}
           onPrev={handlePrev}
           onNext={handleNext}
+          totalPages={totalPages}
         />
-        <Content searchTerm={searchTerm} currentPage={currentPage} />
+        <Content
+          searchTerm={searchTerm}
+          currentPage={currentPage}
+          setTotalPages={setTotalPages}
+          setNotification={setNotification}
+        />
         <Pagination
           currentPage={currentPage}
           onPrev={handlePrev}
